746ef5b493052e68e7054ea8cfc08783be082406
Lasse Fister <commander@graphicore.de>
2015-12-08 22:21:59 +0100

    Setup for live font testing page.

---
+++ html/browserConfig.js
@@ -0,0 +1,10 @@
+require.config({
+    baseUrl: ''
+  , paths: {
+        'require/domReady': 'bower_components/requirejs-domready/domReady'
+      , 'marked': 'bower_components/marked/lib/marked'
+      , 'pako': 'bower_components/pako/dist/pako.min'
+      , 'urlHosted': 'bower_components/urlHosted/lib'
+      , 'ofp': 'bower_components/online-font-project/lib'
+    }
+});
